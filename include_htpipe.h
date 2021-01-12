068de7a526e92f4e11b542a55bac9e4ae135eafc
William Kendrick <rkendric@mit.edu>
2021-01-12 15:29:22 -0500

    Missing files

---
+++ include_htpipe.h
@@ -0,0 +1,53 @@
+// variable_creation
+double t_sink;
+double qtotal;
+double le;
+double lc;
+double la;
+double k_wick;
+double k_wall;
+double radius_in;
+double radius_out;
+double cinc;
+double rbar;
+double wire_radius;
+double annulus_thickness;
+double eff_pore_radius;
+double tlow;
+double thigh;
+double qe_array [] = {0,0,0,0,0,0,0,0,0,0};
+double screen_thickness;
+double rv;
+double av;
+double q_total;
+
+// fluids_properties
+double mw;
+double rk;
+double pv;
+double rho_l;
+double rho_v;
+double mu_v;
+double mu_l;
+double sig;
+double hfg;
+double cfluid;
+
+// dpe_evap
+double dpie;
+double dpve;
+double dple;
+
+// dpa_adiab
+double dpla;
+double dpa;
+
+// dpc_cond
+double dpic;
+double dpvc;
+double dplc;
+
+// sonic_limit
+double q_sonic;
+double pci;
+double tc_sonic;
